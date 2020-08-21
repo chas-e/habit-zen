997cc3eefb9b5d4ea6c492480b18c1af297f8bc5
chas-e <sigmapi76388@gmail.com>
2020-08-21 17:51:12 -0400

    intializes auth, login, signup

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // require modules
 const express = require('express');
 const path = require('path');
-const methodOverride = require('method-override');
+// const methodOverride = require('method-override');
 const favicon = require('serve-favicon');
 const morgan = require('morgan');
 
 
 
 // create app
-const app = express.app();
+const app = express();
 
 // configure app
 require('dotenv').config();
@@ -17,18 +17,21 @@ require('./config/database');
 // mount middleware
 app.use(morgan('dev'));
 app.use(express.json());
-// app.use(favicon(path.join(__dirname, 'build, favicon.ico')));
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 
 
 // mount API routes here, before the "catch-all" route
+app.use('/api/users', require('./routes/api/users'));
+app.use(require('./config/auth'));
 
+// mount  routes we want to protect with auth
 
 // mount "catch-all" route
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'indexedDB.html'));
-// });
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'indexedDB.html'));
+});
 
 
 
