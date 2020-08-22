8aa49d5e41279afb99cd637285c848939de9eb03
ChenguK <ChenguKargbo@gmail.com>
2020-08-22 15:01:27 -0400

    Revert "add NavBar functions"
    This reverts commit 474733f2a5c9354f6c103a6f409d8747afa3e7f4.

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+import NavBar from '../../components/NavBar/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // ...this.getInitialState(),
+      user: userService.getUser()
+    };
+  }
+
+  // getInitialState() {
+  //   return {
+
+  //   };
+  // }
+
+  handleSignupOrLogin = () => {
+    this.setState({ user: userService.getUser() });
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">Welcome to HabitZen</header>
+        <NavBar
+          user={this.state.user}
+          handleLogout={this.handleLogout}
+        />
+        <Switch>
+          <Route exact path="/signup" render={({ history }) =>
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          } />
+          <Route exact path="/login" render={({ history }) =>
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          } />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
