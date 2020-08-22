c60d0402cf2ba5ce7cd003e7d511d02b4e51418f
ChenguK <ChenguKargbo@gmail.com>
2020-08-22 12:06:06 -0400

    Revert "Merge branch 'master' of https://github.com/chas-e/habit-zen"
    This reverts commit 855df8f8a2a25e6a62b447935b5df99e9f385711, reversing
    changes made to a4d10c582822f507239489074240ed7c586fb161.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
-    < Route component={App} />
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
