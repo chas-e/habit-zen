a94d92f42a96fa946dcbc18ba1252edf7a72fedf
ChenguK <ChenguKargbo@gmail.com>
2020-08-25 14:06:15 -0400

    returning to previous App.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -20,9 +20,7 @@ class App extends Component {
       todos: [{ text: '', done: '' , date: ''}],
       habits: [{ 
         goal: '', 
-        habit: '',
-        sDate: '',
-        eDate: ''}],
+        habit: '',}],
       quotes: [],
     };
   }
