f8e97e9c6dd80bada0a196e903ff1850289f42ff
chas-e <sigmapi76388@gmail.com>
2020-08-24 20:46:20 -0400

    adding back overwritten modules

--- routes/api/habits.js
+++ routes/api/habits.js
@@ -1,6 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const habitsCtrl = require('../../controllers/habits');
 
+router.get('/', habitsCtrl.index);
+router.use(require('../../config/auth'));
+router.post('/', checkAuth, habitsCtrl.create);
+
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({
+        msg: "Sorry, but you are not authorized."
+    });
+}
 
 module.exports = router;
--- src/Components/Footer/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Footer = (props) => {
-    console.log(props);
-}
