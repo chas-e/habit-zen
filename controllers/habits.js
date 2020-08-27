38a57051bc212bd2d17ad254736208fa5a090afe
chas-e <sigmapi76388@gmail.com>
2020-08-27 12:50:20 -0400

    renames show to index

--- controllers/habits.js
+++ controllers/habits.js
@@ -11,11 +11,14 @@ module.exports = {
 
 
 async function update(req, res) {
-    await Habit.findByIdAndUpdate(req.params.id, req.body, { new: true },function(err, habit) {
+    await Habit.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, function (err, habit) {
         res.json(habit);
-        
-     })};
-    
+
+    })
+};
+
 
 async function create(req, res) {
     try {
@@ -32,17 +35,16 @@ async function create(req, res) {
 // IDK if this will actually work or not, idea is to get/ render habits associated with the logged in user
 async function index(req, res) {
     const habits = await Habit.find({})
-        // .limit(req.query.limit || 10);
+    // .limit(req.query.limit || 10);
     res.json(habits);
 };
 
 async function deleteHabit(req, res) {
     await Habit.findByIdAndDelete(req.params.id);
- index(req, res);
- }
+    index(req, res);
+}
 
- async function editHabit(req, res) {
+async function editHabit(req, res) {
     await Habit.findByIdAndUpdate(req.params.id, req.body);
-   show(req, res);
- }
- 
+    index(req, res);
+}
