21dae94c66dcc90f220a0caa02f1ac16afab6231
chas-e <sigmapi76388@gmail.com>
2020-08-28 12:30:43 -0400

    fixes https schema

--- src/utils/qrandom-api.js
+++ src/utils/qrandom-api.js
@@ -1,6 +1,6 @@
 export function getRandomQ() {
     // const API_KEY = process.env.REACT_APP_KEY;
-    const url = `http://quotes.rest/quote/random.json?api_key=_yTIPuxE0GiQQcOSMbwuqweF`
+    const url = `https://quotes.rest/quote/random.json?api_key=_yTIPuxE0GiQQcOSMbwuqweF`
     return fetch(url, {
         mode: "cors"
     }).then(res => res.json());
