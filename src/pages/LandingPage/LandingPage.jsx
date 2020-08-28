c504d8df219519ab016e769031f593565a8e85bc
chas-e <sigmapi76388@gmail.com>
2020-08-28 09:35:19 -0400

    removes space on Landing Page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,30 +4,22 @@ import './LandingPage.css';
 
 const LandingPage = (props) => {
     let landing = props.user ?
-            <div className="LandingPage" >
-            <Link to="/newtodo" style={{ color: '#5c90aa', fontSize: '2rem'}}>Add New To Do List Item </Link>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link to="/newhabit" style={{ color: '#5c90aa', fontSize: '2rem'}}>Add New Habits</Link>
+        <div className="LandingPage" >
+            <Link to="/newtodo" style={{ color: '#5c90aa', fontSize: '2rem' }}>Add New To Do List Item </Link>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Link to="/newhabit" style={{ color: '#5c90aa', fontSize: '2rem' }}>Add New Habits</Link>
             <br />
             <br />
-            <Link to="/user" className="LandLink" style={{ fontSize: '3rem', color: '#5c90aa'}}>Click here to see your user profile</Link>
-         </div>
-         :
-           <div>
-           </div>
-         
+            <Link to="/user" className="LandLink" style={{ fontSize: '3rem', color: '#5c90aa' }}>Click here to see your user profile</Link>
+        </div>
+        :
+        <div>
+        </div>
 
-        
-            
-            
-        
+    return (
+        <div>
+            {landing}
+            <p id="title" style={{ fontSize: "8rem", color: '#5c90aa' }}> Welcome to HabitZen</p>
 
-            return (
-                <div>
-                    {landing}
-            <p  id="title" style={{ fontSize: "8rem", color: '#5c90aa'}}> Welcome to HabitZen</p>
-            
-            {/* Need div elements for Goal tracking, habit 'checklist', todo 'checklist' */}
-            
         </div>
     );
 }
