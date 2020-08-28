da07c0b45eefc44a021e1dbbacc1e5f1dd2eb58c
chas-e <sigmapi76388@gmail.com>
2020-08-28 15:59:42 -0400

    adds back comments to the GoalTracker

--- src/components/GoalTracker/GoalTracker.jsx
+++ src/components/GoalTracker/GoalTracker.jsx
@@ -12,17 +12,19 @@ class GoalTracker extends Component {
             today: new Date()
         };
     }
-
+    // this sets a new date object for the current day in state
     handleNewDay = () => {
         const newDay = new Date();
         this.setState({ today: newDay });
     }
 
+    // this calculates the days between the start and end dates entered by the user
     calculateDays = (sDate, eDate) => {
         if (!sDate || !eDate) return;
         return Math.ceil(new Date(eDate) - new Date(sDate) / goalTrackerService.dayInMS());
     }
 
+    // this calculates days left based on today and the goal end date
     calculateDaysLeft = (eDate) => {
         if (!eDate) return;
         return Math.ceil(new Date(eDate) - new Date(this.state.today) / goalTrackerService.dayInMS());
@@ -32,6 +34,7 @@ class GoalTracker extends Component {
         this.handleNewDay();
         this.totalDays = this.calculateDays(this.props.habit.sDate, this.props.habit.eDate);
         this.daysRemaining = this.calculateDaysLeft(this.props.habit.eDate);
+        // this sets the rate for the progress tracker UI based on today and days left
         this.now = Math.abs(1 - this.daysRemaining) / this.totalDays * 100;
     }
 
