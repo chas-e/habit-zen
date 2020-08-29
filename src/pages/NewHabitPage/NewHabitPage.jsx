14fafbcc3f884ecee95466d56f659e63d826520c
chas-e <sigmapi76388@gmail.com>
2020-08-29 09:39:52 -0400

    removes unused import statement

--- src/pages/NewHabitPage/NewHabitPage.jsx
+++ src/pages/NewHabitPage/NewHabitPage.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import NewHabitForm from '../../components/NewHabitForm/NewHabitForm';
-// import habitService from '../../utils/habitService';
 import './NewHabitPage.css';
 
 class NewHabitPage extends Component {
