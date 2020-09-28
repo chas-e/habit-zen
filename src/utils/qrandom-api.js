1f65fd1165ce211a1e30981e76b9e54214f8dc52
chas-e <sigmapi76388@gmail.com>
2020-09-28 18:20:56 -0400

    updating api service module

--- src/utils/qrandom-api.js
+++ src/utils/qrandom-api.js
@@ -1,5 +1,5 @@
 export function getRandomQ() {
-    // const API_KEY = process.env.REACT_APP_KEY;
+    const API_KEY = process.env.REACT_APP_KEY;
     const url = `https://quotes.rest/quote/random.json?`
     return fetch(url, {
         mode: "cors"
